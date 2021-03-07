09ee193bf16008fdc4efda7001a4302b43c4dafe
Jonathan <jonathan.bustamante03@gmail.com>
2021-03-07 00:28:04 -0500

    single_model_orm

---
+++ single_model_orm/users_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class users(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.EmailField(max_length=100)
+    age = models.IntegerField(max_length=45)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
