d35fcb38a30966f520ed9d68d2e770ffc8cb5608
Jonathan <jonathan.bustamante03@gmail.com>
2021-03-08 02:08:56 -0500

    single_model_orm

--- single_model_orm/users_app/models.py
+++ single_model_orm/users_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class users(models.Model):
+class Users(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
     email = models.EmailField(max_length=100)
